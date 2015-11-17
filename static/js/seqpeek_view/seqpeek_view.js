1b8d941c83b143d8701ea52457d4af544ad8213f
kleinone <kleinone@systemsbiology.org>
2015-11-16 17:52:17 -0800

    Added gene autocomplete widget

--- static/js/seqpeek_view/seqpeek_view.js
+++ static/js/seqpeek_view/seqpeek_view.js
@@ -5,6 +5,7 @@ require.config({
         d3: 'vendor/d3.min',
         domReady: 'vendor/domReady',
         jquery: 'vendor/jquery-1.11.1.min',
+        "jquery-ac": 'vendor/jquery-ui-autocomplete.min',
         underscore: 'vendor/underscore',
         vq: 'vendor/vq.min'
     },
@@ -16,11 +17,15 @@ require.config({
 require([
     'domReady',
     './view',
+    'jquery',
+    'jquery-ac',
     'vq'
 
 ], function(
     domReady,
     SeqPeekViewFactory,
+    __jquery,
+    __jquery_ac,
     vq
 ) {
     domReady(function() {
@@ -28,6 +33,12 @@ require([
             console.log("SeqPeek plot data bundle is undefined");
             return;
         }
+
+        $('#genes').autocomplete({
+            source: __data_bundle.gene_list,
+            minLength: 3,
+            delay: 500
+        });
 
         var target_table = $(document).find("#seqpeek-table")[0];
         var tableView = SeqPeekViewFactory.create(target_table, __data_bundle);
