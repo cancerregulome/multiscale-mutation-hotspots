25bb5931adfe90f61ff2522c9e4b603bafa0bece
kleinone <kleinone@systemsbiology.org>
2015-11-16 17:54:01 -0800

    Changed sqlite database name

--- hotspots/settings.py
+++ hotspots/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'hotspots.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'mica.sqlite3'),
     }
 }
 
