69124dba28b8c5ebb9d91fbd2d52ee230c1450ad
kleinone <kleinone@systemsbiology.org>
2015-11-30 17:47:09 -0800

    Do not use Django configuration for database

--- hotspots/database_util.py
+++ hotspots/database_util.py
@@ -2,9 +2,8 @@ from sqlite3 import connect, Row
 from django.conf import settings
 
 def sql_connection():
-    # Connecting to localhost
-    database = settings.DATABASES['default']
-    conn = connect(database['NAME'])
+    # TODO FIX this should be configurable, not hardcoded
+    conn = connect('mica.sqlite3')
     conn.row_factory = Row
 
     return conn
